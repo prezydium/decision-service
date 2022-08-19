830f5185bbcfe98db2c88a74deb8e65967131e44
prezydium <prezydium834@gmail.com>
2022-08-19 10:27:30 +0200

    Initial Commit

---
+++ src/main/java/com/presidium/decisionservice/controller/RandomNumberController.java
@@ -0,0 +1,16 @@
+package com.presidium.decisionservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class RandomNumberController {
+
+    @GetMapping("/random")
+    public Integer getRandomNumber(){
+        return new Random().nextInt(100);
+    }
+
+}
